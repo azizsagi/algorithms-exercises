d730ca9c8277bc70c690b1215f0b18f844f69514
M.F.Aziz <azizsagi@yahoo.com>
2024-01-03 22:06:13 +0000

    # Given a row with money stored in a particular house # Rob the max amount of money # can rob as many houses as we want but we can't rob the two adjacant houses as it will trigger the police # Example: [5,11,4,3] Max will be second home + 4th home = 11 + 3 = 14 the max we can rob
    # Given a row with money stored in a particular house
    # Rob the max amount of money
    # can rob as many houses as we want but we can't rob the two adjacant houses as it will trigger the police
    # Example: [5,11,4,3] Max will be second home + 4th home = 11 + 3 = 14 the max we can rob

---
+++ rob-house-money.py
@@ -0,0 +1,27 @@
+# Given a row with money stored in a particular house
+# Rob the max amount of money
+# can rob as many houses as we want but we can't rob the two adjacant houses as it will trigger the police
+# Example: [5,11,4,3] Max will be second home + 4th home = 11 + 3 = 14 the max we can rob 
+
+def rob_home(homes):
+    l = len(homes)  # The length of the given homes
+
+    if l==1: return homes[0]
+    if l==2: return max(homes)
+
+    maxx = [homes[0], max(homes[0], homes[1])]
+
+    for i in range(2, l):
+        curr = max(homes[i] + maxx[i-2], maxx[i-1])
+        maxx.append(curr)
+
+    return maxx[-1]
+
+# Test cases
+
+print(rob_home([5,10,3,4])) #Output = 14
+print(rob_home([1, 2, 3, 4, 5, 6, 7]))  #Output = 16
+
+
+
+
